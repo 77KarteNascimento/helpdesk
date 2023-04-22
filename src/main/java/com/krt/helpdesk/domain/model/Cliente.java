1636d54424d8d47a712f679ae7b69f24eefdbc4e
Karte Nascimento <ti.kartenascimento@gmail.com>
2023-04-21 20:05:54 -0400

    adicionando as anotações JPA ao pacote de dominio

---
+++ src/main/java/com/krt/helpdesk/domain/model/Cliente.java
@@ -0,0 +1,37 @@
+package com.krt.helpdesk.domain.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.krt.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
+	private List<Chamado> chamados = new ArrayList<>();
+
+	public Cliente() {
+		addPerfil(Perfil.CLIENTE);//assim que o cliente for adicionado ele ja vem com o perfil de cliente
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+		
+		addPerfil(Perfil.CLIENTE);
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+	
+	
+}
--- src/main/java/com/krt/helpdesk/domain/model/Pessoa.java
+++ src/main/java/com/krt/helpdesk/domain/model/Pessoa.java
@@ -1,20 +1,48 @@
-package com.krt.helpdesk.domain;
+package com.krt.helpdesk.domain.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.krt.helpdesk.domain.enums.Perfil;
 
-public abstract class Pessoa {
+@Entity
+public abstract class Pessoa implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	protected Integer id;
 	protected String nome;
+	
+	@CPF
+	@Column(unique = true)
 	protected String cpf;
+	
+	@Column(unique = true)
 	protected String email;
+	
 	protected String senha;
+	
+	@ElementCollection(fetch = FetchType.EAGER)//assegurando que a minha lista de perfis venha junto com o usuario
+	@CollectionTable(name = "PERFIS")
 	protected Set<Integer> perfis = new HashSet<>();
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate dataCriacao = LocalDate.now();
 	
 	
