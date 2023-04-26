9bd9fcaa0d79b192f8e05ee470008092c04766e7
Karte Nascimento <ti.kartenascimento@gmail.com>
2023-04-25 21:23:28 -0400

    Criando o endpoint de buscar todos os tecnicos

--- src/main/java/com/krt/helpdesk/service/TecnicoService.java
+++ src/main/java/com/krt/helpdesk/service/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.krt.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = tecnicoRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id ));
+	}
+
+
+	public List<Tecnico> findAll() {
+		return tecnicoRepository.findAll();
 	}
 
 }
